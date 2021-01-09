364d40318966ba76a9209f59ebdf36ead2481ce7
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2021-01-09 18:54:49 +0900

    fix result page

--- gradient/app.js
+++ gradient/app.js
@@ -1,81 +1,15 @@
 import { GlowParticle } from "./gradient.js";
 
 const optionBtn = document.querySelectorAll("button");
+const main = document.querySelector("main");
 const explanation = document.querySelector("h2");
 const question = document.querySelector("h1");
 const options = document.querySelector("div.options");
 const shareBtn = document.querySelector("#kakao-link-btn");
+const backGround = document.querySelector(".background");
+
 let score = 0;
 let click = 0;
-
-const COLORS = [
-    {r :240, g: 161, b:118},
-    {r :247, g: 77, b:153},
-    {r :138, g: 81, b:224},
-    {r :77, g: 159, b:247},
-    {r :74, g: 237, b:175}
-];
-
-class App {
-    constructor() {
-        this.canvas = document.createElement('canvas');
-        document.body.appendChild(this.canvas);
-        this.ctx = this.canvas.getContext('2d');
-        
-        this.pixelRatio = (window.devicePixelRatio > 1) ? 2 : 1;
-
-        this.totalParticles = 15;
-        this.particles = [];
-        this.maxRadius = 800;
-        this.minRadius = 400;
-
-        window.addEventListener('resize', this.resize.bind(this), false);
-        this.resize();
-
-        window.requestAnimationFrame(this.animate.bind(this));
-    }
-    resize() {
-        this.stageWidth = document.body.clientWidth;
-        this.stageHeight = document.body.clientHeight;
-    
-        this.canvas.width = this.stageWidth * this.pixelRatio;
-        this.canvas.height = this.stageHeight * this.pixelRatio;
-        this.ctx.scale(this.pixelRatio, this.pixelRatio);
-
-        // this.ctx.globalCompositeOperation = 'saturation';
-        this.createParticles();
-    }
-    createParticles() {
-        let curColor = 0;
-        this.particles=[];
-
-        for (let i = 0; i < this.totalParticles; i++) {
-            const item = new GlowParticle(
-                Math.random() * this.stageWidth,
-                Math.random() * this.stageHeight,
-                Math.random() * (this.maxRadius - this.minRadius) + this.minRadius,
-                COLORS[curColor]
-            );
-            
-            if (++curColor >= COLORS.length) {
-                curColor = 0;
-            }
-
-            this.particles[i] = item;
-        }
-    }
-    animate() {
-        window.requestAnimationFrame(this.animate.bind(this));
-
-        this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
-        for (let i = 0; i < this.totalParticles; i++) {
-            const item = this.particles[i];
-            item.animate(this.ctx, this.stageWidth, this.stageHeight);
-
-            
-        }
-    }
-}
 
 // 각 화면
 function sectionFirst(){
@@ -175,36 +109,115 @@ function removeAllChild(array) {
         array.removeChild(array.firstChild);
     }
 }
+
 function resultFirst(){
     question.innerHTML = "끝1";
-    
+    backGround.style.filter = "opacity(0.9)";
 }
 function resultSecond(){
     question.innerHTML = "끝2";
+    backGround.style.filter = "opacity(0.8)";
 }
 function resultThird(){
     question.innerHTML = "끝3";
+    backGround.style.filter = "opacity(0.7)";
 }
 function resultFourth(){
     question.innerHTML = "끝4";
+    backGround.style.filter = "opacity(0.6)";
 }
 function resultFifth(){
     question.innerHTML = "끝5";
+    backGround.style.filter = "opacity(0.5)";
 }
 function resultSixth(){
     question.innerHTML = "끝6";
+    backGround.style.filter = "opacity(0.4)";
 }
 function resultSeventh(){
     question.innerHTML = "끝7";
+    backGround.style.filter = "opacity(0.2)";
 }
 function resultEighth(){
     question.innerHTML = "끝8";
+    backGround.style.filter = "opacity(0)";
+}
+
+const COLORS = [
+    {r :240, g: 161, b:118},
+    {r :247, g: 77, b:153},
+    {r :138, g: 81, b:224},
+    {r :77, g: 159, b:247},
+    {r :74, g: 237, b:175}
+];
+
+class App {
+    constructor() {
+        this.canvas = document.createElement('canvas');
+        document.body.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext('2d');
+        
+        this.pixelRatio = (window.devicePixelRatio > 1) ? 2 : 1;
+
+        this.totalParticles = 15;
+        this.particles = [];
+        this.maxRadius = 800;
+        this.minRadius = 400;
+
+        window.addEventListener('resize', this.resize.bind(this), false);
+        this.resize();
+
+        window.requestAnimationFrame(this.animate.bind(this));
+    }
+    resize() {
+        this.stageWidth = document.body.clientWidth;
+        this.stageHeight = document.body.clientHeight;
+    
+        this.canvas.width = this.stageWidth * this.pixelRatio;
+        this.canvas.height = this.stageHeight * this.pixelRatio;
+        this.ctx.scale(this.pixelRatio, this.pixelRatio);
+
+        // this.ctx.globalCompositeOperation = 'saturation';
+        this.createParticles();
+    }
+    createParticles() {
+        let curColor = 0;
+        this.particles=[];
+
+        for (let i = 0; i < this.totalParticles; i++) {
+            const item = new GlowParticle(
+                Math.random() * this.stageWidth,
+                Math.random() * this.stageHeight,
+                Math.random() * (this.maxRadius - this.minRadius) + this.minRadius,
+                COLORS[curColor]
+            );
+            
+            if (++curColor >= COLORS.length) {
+                curColor = 0;
+            }
+
+            this.particles[i] = item;
+        }
+    }
+    animate() {
+        window.requestAnimationFrame(this.animate.bind(this));
+
+        this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+        for (let i = 0; i < this.totalParticles; i++) {
+            const item = this.particles[i];
+            item.animate(this.ctx, this.stageWidth, this.stageHeight);
+
+            
+        }
+    }
 }
 
 //점수 계산
 function calculator() {
     if (click == 0) {
         sectionFirst();
+        optionBtn[1].classList.remove("hide");
+        optionBtn[2].classList.remove("hide");
         click +=1;
     } else if (click == 1) {
         sectionSecond();
@@ -232,7 +245,10 @@ function calculator() {
         click +=1;
     } else if (click ==10) {
         new App();
-        removeAllChild(options)
+        removeAllChild(options);
+        backGround.classList.remove("hide");
+        main.style.color = "white";
+        //appendChild
         if(score >= 36) {
             resultFirst();
         } else if (score >= 32) {
